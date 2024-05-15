145cddbde2fbb79d11309f8503d9b16d8bdef4ec
Dimitri Sluzki <dimitri.sluzki@haas.de>
2024-05-15 11:33:20 +0200

    Start

--- ohaCalendar/calendar.cs
+++ ohaCalendar/calendar.cs
@@ -256,7 +256,6 @@ namespace ohaCalendar
                 //await RefreshHolidays(true);
                 //StateToolStripComboBox_SelectedValueChanged(null, null);
 
-
                 tableLayoutPanel1.Controls.Clear();
                 tableLayoutPanel2.Controls.Clear();
                 tableLayoutPanel3.Controls.Clear();
